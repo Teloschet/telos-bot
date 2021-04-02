2cad2714c70aa16e81476eab826a39d3052efb3e
Teloschet <frapgameroficial@hotmail.com>
2021-04-02 19:06:55 -0300

    atualization in ban command

--- comandos/ban.js
+++ comandos/ban.js
@@ -3,5 +3,4 @@ const Discord = require("discord.js");
 exports.run = (bot, message, args) => {
     if(!message.member.hasPermission("BAN_MEMBERS")) return message.reply("Você não tem permissão!");
     if(args.length === 0) return message.reply("utilize !ban <@usuário> <motivo>");
-    let banMember = message
 }
